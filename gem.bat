6efe0d32ff959ed53ccda7a1b30372743e793984
hirof <hirofumi@ukawa.biz>
2026-05-15 20:19:18 +0900

    Remove local env scripts and ignore them

--- gem.bat
+++
@@ -1,4 +0,0 @@
-curl "https://aiplatform.googleapis.com/v1/publishers/google/models/gemini-2.5-flash:streamGenerateContent?key=%UAGENT_VERTEXAI_API_KEY%" ^
-  -X POST ^
-  -H "Content-Type: application/json" ^
-  -d "{\"contents\":[{\"role\":\"user\",\"parts\":[{\"text\":\"Explain how AI works in a few words\"}]}]}"
